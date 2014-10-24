c9a11c415e84f1e33790cbe69769785b83996d61
Mark Tranter <mark.a.tranter@gmail.com>
2014-10-24 13:13:13 +0100

    Add Appveyor nuget packaging

--- CypherNet/Properties/AssemblyInfo.cs
+++ CypherNet/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.2.6")]
-[assembly: AssemblyFileVersion("0.9.2.6")]
+[assembly: AssemblyVersion("0.9.2.7")]
+[assembly: AssemblyFileVersion("0.9.2.7")]
 [assembly: InternalsVisibleTo("CypherNet.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
