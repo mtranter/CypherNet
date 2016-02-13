b7f439c9e1d3f6997768e1b2581226d6df4691b3
mtranter <mark.a.tranter@gmail.com>
2016-02-13 08:45:55 +0000

    Install with ps

---
+++ setup-neo4j.ps1
@@ -0,0 +1,19 @@
+Import-Module "c:\projects\CypherNet\neo4j-community-2.3.2\bin\Neo4j-Management.psd1"
+
+function update-password
+{
+  $user = "neo4j"
+  $pass= "neo4j"
+  $uri = "http://localhost:7474/user/neo4j/password"
+  $json = "{
+            ""password"" : ""password""
+          }"
+  
+  $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$pass)))
+
+  Invoke-RestMethod -Uri $uri -Method Post -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "application/json; charset=UTF-8" -Body $json
+}
+ 
+Install-Neo4jServer -Neo4jServer "c:\projects\CypherNet\neo4j-community-2.3.2" -PassThru | Start-Neo4jServer -Wait
+
+update-password
